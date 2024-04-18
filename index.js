847d5f888aff518ae9abd8c7fce8b5ae2918cca0
Thalavignesh24 <82791388+Thalavignesh24@users.noreply.github.com>
2024-04-18 20:56:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+
+
+app.get("/vpn-detect",(req,res)=>{
+  console.log(req?.headers);
+   const interfaces = os.networkInterfaces();
+   console.log(interfaces)
+         for (const interfaceName in interfaces) {
+             const interfaceInfo = interfaces[interfaceName];
+             if (interfaceInfo) {
+                 for (const interfaceDetail of interfaceInfo) {
+                     if (interfaceDetail.family === 'IPv4' && !interfaceDetail.internal) {
+                         if (interfaceDetail.netmask === '255.255.255.255') {
+                             console.log(true);
+                             return res.send({
+                               message:interfaces
+                             });
+                         }
+                     }
+                 }
+             }
+         }
+         console.log(false);
+         return res.send({
+           message:interfaces
+         })
+});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
